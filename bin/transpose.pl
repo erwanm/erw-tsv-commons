1d222b7841fafceca9ad1e783e10d51ef009b304
Erwan Moreau <moreaue@scss.tcd.ie>
2016-12-15 21:36:30 +0000

    added option format to transpose

--- bin/transpose.pl
+++ bin/transpose.pl
@@ -5,12 +5,18 @@ use warnings;
 use Getopt::Std;
 
 
+my $printfPattern="%s";
+
 sub usage {
 	my $fh = shift;
 	$fh = *STDOUT if (!defined $fh);
 	print $fh "Usage: transpose.pl\n";
 	print $fh "  reads from STDIN unless arg is provided, writes to STDOUT.\n";
 	print $fh "  transposes the table: columns become rows and conversely.\n";
+	print $fh "\n";
+	print $fh " Options:\n";
+	print $fh "  -f <printf format> printf format for  a value, \n";
+	print $fh "     default='$printfPattern'.\n";
 	print $fh "\n";
 }
 
@@ -20,9 +26,10 @@ sub usage {
 
 # PARSING OPTIONS
 my %opt;
-getopts('h', \%opt ) or  ( print STDERR "Error in options" &&  usage(*STDERR) && exit 1);
+getopts('hf:', \%opt ) or  ( print STDERR "Error in options" &&  usage(*STDERR) && exit 1);
 usage($STDOUT) && exit 0 if $opt{h};
 print STDERR "0 arguments expected but ".scalar(@ARGV)." found: ".join(" ; ", @ARGV)  && usage(*STDERR) && exit 1 if (scalar(@ARGV) > 0);
+$printfPattern=$opt{f} if (defined($opt{f}));
 
 my @table;
 my $nbCols = undef;
@@ -41,9 +48,9 @@ while (<STDIN>) {
 }
 
 for (my $i=0; $i<$nbCols; $i++) {
-    print $table[0]->[$i];
+    printf($printfPattern, $table[0]->[$i]);
     for (my $j=1; $j<scalar(@table); $j++) {
-	print "\t".$table[$j]->[$i];
+	printf( "\t".$printfPattern, $table[$j]->[$i]);
     }
     print "\n";
 }
